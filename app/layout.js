353f6a12d9b5f0bfb27a363ad654021d14987871
beyandju <jokdio77@gmail.com>
2026-01-28 23:11:31 +0000

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,1 +1,15 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "Les Jolies Colonies de Vacances",
+  description: "Colonies de vacances – Côte d’Ivoire, Afrique & Europe",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="fr">
+      <body>{children}</body>
+    </html>
+  );
+}
 
